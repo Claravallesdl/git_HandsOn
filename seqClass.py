14b45ae311b151d56cb9326a595dd099d72f4ebe
Your Name <clara.valles@uvic.cat>
2024-02-18 18:19:00 +0100

    Fix classification of DNA and RNA sequences

--- seqClass.py
+++ seqClass.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys
+import re
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
-parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
-parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+def classify_sequence(sequence):
+    sequence = sequence.upper()
+    if re.search('^[ACGT]+$', sequence):
+        return 'DNA'
+    elif re.search('^[ACGU]+$', sequence):
+        return 'RNA'
+    else:
+        return 'not DNA nor RNA'
+
+parser = ArgumentParser(description='Classify a sequence as DNA or RNA')
+parser.add_argument("-s", "--seq", type=str, required=True, help="Input sequence")
+parser.add_argument("-m", "--motif", type=str, required=False, help="Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -13,21 +23,15 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq.upper()       
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
-    else:
-        print ('The sequence can be DNA or RNA')
-else:
-    print ('The sequence is not DNA nor RNA')
+sequence_type = classify_sequence(args.seq)
+
+print(f'The sequence is {sequence_type}')
 
 if args.motif:
-    args.motif = args.motif.upper()
-    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
-    if re.search(args.motif, args.seq):
+    motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{motif}" in sequence "{args.seq}"... ', end='')
+    if re.search(motif, args.seq.upper()):
         print("Found")
     else:
-        print("NOT F")
+        print("NOT FOUND")
+
